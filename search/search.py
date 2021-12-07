40451fedffd3dcee0719e6b4358e29ba00982f75
Faisal <hfaisal1@asu.edu>
2021-12-07 14:18:18 -0700

    added search functions

--- search/search.py
+++ search/search.py
@@ -88,110 +88,107 @@ def depthFirstSearch(problem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
+    new_found = []
+    expanded = []
+    # start state here
+    startstate = problem.getStartState()
+    startnode = (startstate, [])
+    new_found.append(startnode)
+    while len(new_found) >= 0:
+        currentstate, actions = new_found[len(new_found) - 1]
+        new_found.pop(-1)
 
-    if(problem.isGoalState(problem.getStartState())):
-        return []
-    initialState=problem.getStartState()
-    node = None
-    frontierStack = util.Stack()
-    frontierStack.push(Node(initialState))
-    closedSet = set()
-    failure = 0
-    while 5>0:
-        if(frontierStack.isEmpty()):
-            failure = 1 
-        node = frontierStack.pop()
-        nodeState = node.state
-        nodeActions = node.action
-        if(problem.isGoalState(nodeState)):
-            break
-        if nodeState not in closedSet:
-            closedSet.add(nodeState)
-            childNodes = problem.getSuccessors(nodeState)
+        if currentstate not in expanded:
+            expanded.append(currentstate)
 
-            for childNode in childNodes:
-                fullActions = [*nodeActions,childNode[1]]
-                tempNode=Node(childNode[0],fullActions)
-                frontierStack.push(tempNode)
-         
-    if failure == 1:
-        print("failure")
-        return []
-    
-    return nodeActions
-    util.raiseNotDefined()
+            if problem.isGoalState(currentstate):
+                return actions
+            else:
+                successors = problem.getSuccessors(currentstate)
+
+                for succState, succAction, succCost in successors:
+                    newaction = actions + [succAction]
+                    newnode = (succState, newaction)
+                    new_found.append(newnode)
 
 def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    if(problem.isGoalState(problem.getStartState())):
-        return []
-    initialState=problem.getStartState()
-    node = None
-    frontierStack = util.Queue()
-    frontierStack.push(Node(initialState))
-    closedSet = set()
-    failure = 0
-    while 5>0:
-        if(frontierStack.isEmpty()):
-            failure = 1 
-        node = frontierStack.pop()
-        nodeState = node.state
-        nodeActions = node.action
-        if(problem.isGoalState(nodeState)):
-            break
-        if nodeState not in closedSet:
-            closedSet.add(nodeState)
-            childNodes = problem.getSuccessors(nodeState)
+    q = util.Queue()
 
-            for childNode in childNodes:
-                fullActions = [*nodeActions,childNode[1]]
-                tempNode=Node(childNode[0],fullActions)
-                frontierStack.push(tempNode)
-         
-    if failure == 1:
-        print("failure")
-        return []
-    
-    return nodeActions
-    util.raiseNotDefined()
+    # visitedPos holds all of the visited positions already (this is required for
+    # the graph-search implementation of BFS)
+    visitedPos = []
+
+    # push starting state onto the stack with an empty path
+    q.push((problem.getStartState(), []))
+
+    # Then we can start looping, note our loop condition is if the stack is empty
+    # if the stack is empty at any point we failed to find a solution
+    while (not q.isEmpty()):
+
+        # since our stack elements contain two elements
+        # we have to fetch them both like this
+        currentPos, currentPath = q.pop()
+        # print("Currently Visiting:", currentPos, "\nPath=", end="");
+        # print(currentPath);
+        # then we append the currentPos to the list of visited positions
+        visitedPos.append(currentPos)
+
+        # check if current state is a goal state, if it is, return the path
+        if (problem.isGoalState(currentPos)):
+            return currentPath
+
+        # obtain the list of successors from our currentPos
+        successors = problem.getSuccessors(currentPos)
+
+        # if we have successors, note that these successors have a position and the path to get there
+        if (len(successors) != 0):
+            # iterate through them
+            for state in successors:
+                # if we find one that has not already been visisted
+                if ((state[0] not in visitedPos) and (state[0] not in (stateQ[0] for stateQ in q.list))):
+                    # calculate the new path (currentPath + path to reach new state's position)
+                    newPath = currentPath + [state[1]]
+                    # push it onto the stack with the new path
+                    q.push((state[0], newPath))
+
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    if(problem.isGoalState(problem.getStartState())):
-        return []
-    initialState=problem.getStartState()
-    node = None
-    frontierStack = util.PriorityQueue()
-    frontierStack.push(Node(initialState),0)
-    closedSet = set()
-    failure = 0
-    while 5>0:
-        if(frontierStack.isEmpty()):
-            failure = 1 
-        node = frontierStack.pop()
-        nodeState = node.state
-        nodeActions = node.action
-        nodeCosts = node.cost
-        if(problem.isGoalState(nodeState)):
-            break
-        if nodeState not in closedSet:
-            closedSet.add(nodeState)
-            childNodes = problem.getSuccessors(nodeState)
+    q = util.PriorityQueue()
 
-            for childNode in childNodes:
-                fullActions = [*nodeActions,childNode[1]]
-                fullCosts = nodeCosts+childNode[2]
-                tempNode=Node(childNode[0],fullActions,fullCosts)
-                frontierStack.push(tempNode,fullCosts)
-         
-    if failure == 1:
-        print("failure")
-        return []
-    
-    return nodeActions
-    util.raiseNotDefined()
+    visitedPos = []
+
+    q.push((problem.getStartState(), []), 0)
+
+    while (not q.isEmpty()):
+
+        currentPos, currentPath = q.pop()
+
+        visitedPos.append(currentPos)
+
+        if (problem.isGoalState(currentPos)):
+            return currentPath
+
+        successors = problem.getSuccessors(currentPos)
+
+        if (len(successors) != 0):
+            for state in successors:
+                if (state[0] not in visitedPos) and (state[0] not in (stateQ[2][0] for stateQ in q.heap)):
+                    newPath = currentPath + [state[1]]
+                    q.push((state[0], newPath), problem.getCostOfActions(newPath))
+
+                elif (state[0] not in visitedPos) and (state[0] in (stateQ[2][0] for stateQ in q.heap)):
+                    for stateQ in q.heap:
+                        if stateQ[2][0] == state[0]:
+                            oldPriority = problem.getCostOfActions(stateQ[2][1])
+
+                    newPriority = problem.getCostOfActions(currentPath + [state[1]])
+
+                    # State is cheaper with his hew father -> update and fix parent #
+                    if oldPriority > newPriority:
+                        newPath = currentPath + [state[1]]
+                        q.update((state[0], newPath), newPriority)
 
 def nullHeuristic(state, problem=None):
     """
@@ -200,43 +197,84 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
+from util import PriorityQueue
+
+
+class PriorityQ_and_Function(PriorityQueue):
+    """
+    Implements a priority queue with the same push/pop signature of the
+    Queue and the Stack classes. This is designed for drop-in replacement for
+    those two classes. The caller has to provide a priority function, which
+    extracts each item's priority.
+    """
+
+    def __init__(self, problem, priorityFunc):
+        "priorityFunction (item) -> priority"
+        self.priorityFunc = priorityFunc  # store the priority function
+        PriorityQueue.__init__(self)  # super-class initializer
+        self.problem = problem
+
+    def push(self, item, heuristic):
+        "Adds an item to the queue with priority from the priority function"
+        PriorityQueue.push(self, item, self.priorityFunc(self.problem, item, heuristic))
+
+
+# Calculation of f(n) = g(n) + h(n)
+def f(problem, state, heuristic):
+    return problem.getCostOfActions(state[1]) + heuristic(state[0], problem)
+
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    if(problem.isGoalState(problem.getStartState())):
-        return []
-    initialState=problem.getStartState()
-    node = None
-    frontierStack = util.PriorityQueue()
-    frontierStack.push(Node(initialState),0)
-    closedSet = set()
-    failure = 0
-    while True:
-        if(frontierStack.isEmpty()):
-            failure = 1 
-        node = frontierStack.pop()
-        nodeState = node.state
-        nodeActions = node.action
-        nodeCosts = node.cost
-        if(problem.isGoalState(nodeState)):
-            break
-        if nodeState not in closedSet:
-            closedSet.add(nodeState)
-            childNodes = problem.getSuccessors(nodeState)
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    queueXY = PriorityQ_and_Function(problem, f)
 
-            for childNode in childNodes:
-                fullActions = [*nodeActions,childNode[1]]
-                fullCosts = nodeCosts+childNode[2]
-                tempNode=Node(childNode[0],fullActions,fullCosts)
-                itemCost = fullCosts + heuristic(childNode[0],problem) #applying heuristic
-                frontierStack.push(tempNode,itemCost)
-         
-    if failure == 1:
-        print("failure")
+    path = []  # Every state keeps it's path from the starting state
+    visited = []  # Visited states
+
+    # Check if initial state is goal state #
+    if problem.isGoalState(problem.getStartState()):
         return []
-    
-    return nodeActions
-    util.raiseNotDefined()
+
+    # Add initial state. Path is an empty list #
+    element = (problem.getStartState(), [])
+
+    queueXY.push(element, heuristic)
+
+    while (True):
+
+        # Terminate condition: can't find solution #
+        if queueXY.isEmpty():
+            return []
+
+        # Get informations of current state #
+        xy, path = queueXY.pop()  # Take position and path
+
+        # State is already been visited. A path with lower cost has previously
+        # been found. Overpass this state
+        if xy in visited:
+            continue
+
+        visited.append(xy)
+
+        # Terminate condition: reach goal #
+        if problem.isGoalState(xy):
+            return path
+
+        # Get successors of current state #
+        succ = problem.getSuccessors(xy)
+
+        # Add new states in queue and fix their path #
+        if succ:
+            for item in succ:
+                if item[0] not in visited:
+                    # Like previous algorithms: we should check in this point if successor
+                    # is a goal state so as to follow lectures code
+
+                    newPath = path + [item[1]]  # Fix new path
+                    element = (item[0], newPath)
+                    queueXY.push(element, heuristic)
 
 
 class Node(object):
